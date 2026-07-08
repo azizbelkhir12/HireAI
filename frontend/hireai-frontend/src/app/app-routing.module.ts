61994e553dcf4a354e40f67d03d32641166472a6
azizbelkhir12 <mouhamedazizbelkhir20@gmail.com>
2026-07-08 12:12:58 +0100

    feat: add recruiter applicants component and update routing

--- frontend/hireai-frontend/src/app/app-routing.module.ts
+++ frontend/hireai-frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './register/register.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { StatCardComponent } from './stat-card/stat-card.component';
 import { RecruiterDashboardComponent } from './recruiter-dashboard/recruiter-dashboard.component';
+import { RecruiterApplicantsComponent } from './recruiter-applicants/recruiter-applicants.component';
 
 
 const routes: Routes = [
@@ -14,7 +15,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path : 'register', component: RegisterComponent },
   { path: 'verify-email', component: VerifyEmailComponent },
-  {path : 'recruiter-dashboard', component: RecruiterDashboardComponent}
+  { path : 'recruiter-dashboard', component: RecruiterDashboardComponent},
+  { path: 'recruiter-applicants', component: RecruiterApplicantsComponent },
   
 ];
 
